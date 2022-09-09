9cdd33a7c16f60d194270014f14797332e610ab1
bgerber123 <bgerber@uri.edu>
2022-09-09 16:43:43 -0400

    updates

--- purl.script.r
+++ purl.script.r
@@ -1,2 +1,5 @@
 setwd("C:/Users/bgerber/Google Drive/GITHUB/bgerber123.github.io")
+
 knitr::purl("./Week 1/Information.qmd",output="./Week 1/Information.R")
+
+knitr::purl("./Week 2/StudyDesign.qmd",output="./Week 2/StudyDesign.R")
