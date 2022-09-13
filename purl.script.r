2dbf98868c5bb09a69fb76fa94075bfd4d6ac37f
bgerber123 <bgerber@uri.edu>
2022-09-13 15:06:11 -0400

    major update

--- purl.script.r
+++ purl.script.r
@@ -2,8 +2,8 @@ setwd("C:/Users/bgerber/Google Drive/GITHUB/bgerber123.github.io")
 
 knitr::purl("./Week 1/Information.qmd",output="./Week 1/Information.R")
 
-knitr::purl("./Week 2/StudyDesign.qmd",output="./Week 2/StudyDesign.R")
+knitr::purl("./Week 3/StudyDesign.qmd",output="./Week 3/StudyDesign.R")
 
-knitr::purl("./Week 3/Probability.qmd",output="./Week 3/Probability.R")
+knitr::purl("./Week 4/Probability.qmd",output="./Week 4/Probability.R")
 
-knitr::purl("./Week 4/linear.model.qmd",output="./Week 4/linear.model.R")
+knitr::purl("./Week 5/linear.model.qmd",output="./Week 5/linear.model.R")
