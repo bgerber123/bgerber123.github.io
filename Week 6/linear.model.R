3002e38f77c2695408d2f77da8bdbebbedbe912e
bgerber123 <bgerber@uri.edu>
2022-10-24 13:41:39 -0400

    update

--- Week 6/linear.model.R
+++ Week 6/linear.model.R
@@ -1,10 +1,7 @@
-## ---- eval=TRUE, echo=TRUE---------------------------------------
+## ---- eval=TRUE, echo=FALSE--------------------------------------
 # x variable (independent and known)
   x = seq(0,2,by=0.1)
-  x
 
-
-## ---- eval=TRUE, echo=TRUE---------------------------------------
 # marginal coefficients 
   beta0 = 1
   beta1 = 0.5
@@ -29,7 +26,7 @@
   lines(x,mu,lwd=3)
 
 
-## ---- eval=TRUE, echo=TRUE---------------------------------------
+## ---- eval=TRUE, echo=FALSE--------------------------------------
 #sample many times  
   y.many = replicate(1000,rnorm(length(mu),mu, sigma))
   dim(y.many)
@@ -38,11 +35,16 @@
 ## ---- eval=TRUE, echo=FALSE--------------------------------------
 #plot all samples with true mu
   par(cex.axis=1.3,cex.lab=1.3)
-  matplot(y.many,type="p",pch=18,xaxt="n",col=4,cex=2,xlab="x",ylab="y",ylim=c(-2,4))
+  matplot(y.many,type="p",pch=18,xaxt="n",col=2,cex=2,xlab="x",ylab="y",ylim=c(-2,4))
   axis(1,at=1:length(x),lab=x)
   lines(1:length(x),mu,lwd=3)
   points(1:length(x),y,type="p",col=4,lwd=4,ylim=c(-2,3),
        pch=18,cex=2)
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+y <- matrix(c(1,2,3),nrow=1,ncol=3)
+y
 
 
 ## ---- eval=TRUE, echo=TRUE---------------------------------------
@@ -87,13 +89,21 @@ dim(second)
 
 
 ## ---- eval=TRUE, echo=TRUE---------------------------------------
+t(y)%*%y
+
+y%*%t(y)
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 # GLM coefs
-  coef(glm(y~x))
+  eleph=read.csv("elephant.study.csv")
+  X=model.matrix(~sex+age.years,data=eleph)
+  coef(glm(weight~0+X,data=eleph))
 
 
 ## ---- eval=TRUE, echo=TRUE---------------------------------------
 # Linear Algebra Coefs
-  X=model.matrix(~x)
+  y=eleph$weight
   c(solve(t(X)%*%X)%*%t(X)%*%y)
 
 
@@ -104,6 +114,7 @@ dim(second)
   p=0.2+0.3*x+rnorm(n,0,0.05)
   plot(x,p,xlab="Variable",ylab="Probability",
        ylim=c(-0.5,1.5),xlim=c(-2,3),cex=1.5)
+    abline(h=c(0,1),lwd=3,col='purple')
 
 
 ## ---- eval=TRUE, echo=FALSE--------------------------------------
@@ -111,7 +122,14 @@ dim(second)
        ylim=c(-0.5,1.5),xlim=c(-2,3),cex=1.5)
   abline(h=c(0,1),lwd=3,col='purple')
 
-  abline(glm(p~x),lwd=3,col=2)
+  #abline(glm(p~x),lwd=3,col=2)
+  newdata=data.frame(x=seq(-1,3,by=0.25))
+  a=predict(glm(p~x),newdata = newdata,se.fit = TRUE)
+  LCL=a$fit-a$se.fit*qnorm(0.025)
+  UCL=a$fit-a$se.fit*qnorm(0.975)
+  lines(newdata$x,a$fit,lwd=3,col=2)
+  lines(newdata$x,LCL,lwd=3,col=3)
+  lines(newdata$x,UCL,lwd=3,col=3)
 
 
 ## ---- eval=TRUE,echo=TRUE----------------------------------------
@@ -183,10 +201,12 @@ extract_eq(model1)
        main=bquote("Sampling Distribution"~beta[0]),
        xlab=bquote(beta[0]))
       abline(v=beta[1],col=2,lwd=4)
+      legend("topright",lwd=3,col=2,legend="True Value")
   plot(density(coef.est[2,],adjust=2),type="l",lwd=2,
        main=bquote("Sampling Distribution"~beta[1]),
        xlab=bquote(beta[1]))
   abline(v=beta[2],col=2,lwd=4)
+    legend("topright",lwd=3,col=2,legend="True Value")
 
 
 ## ---- eval=TRUE,echo=TRUE, fig.align='center'--------------------
@@ -223,7 +243,7 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
 
 
 ## ---- eval=TRUE,echo=TRUE, fig.align='center'--------------------
-#Plot the linear 'terms'  and probability
+#Plot the linear 'terms'  and explantory variable
   par(cex.lab=1.2,cex.axis=1.2)
   plot(Var1,lt,type="b",lwd=3,col=2,
        xlab="x",ylab="Linear Terms (logit-value)")  
@@ -259,6 +279,7 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
        main=bquote("Sampling Distribution"~beta[0]),
        xlab=bquote(beta[0]))
       abline(v=beta[1],col=2,lwd=4)
+            legend("topright",lwd=3,col=2,legend="True Value")
 
 
 ## ---- eval=TRUE, echo=FALSE--------------------------------------
@@ -266,6 +287,7 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
        main=bquote("Sampling Distribution"~beta[1]),
        xlab=bquote(beta[0]))
   abline(v=beta[2],col=2,lwd=4)
+        legend("topright",lwd=3,col=2,legend="True Value")
 
 
 ## ---- eval=TRUE, echo=TRUE---------------------------------------
@@ -287,7 +309,7 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
 
 ## ---- eval=TRUE, echo=TRUE---------------------------------------
 # Probability of estimating the slope within 1
-    length(which(coef.est[2,]>=beta[2]-1 & coef.est[2,]<=beta[2]+1))/n.sim
+length(which(coef.est[2,]>=beta[2]-1 & coef.est[2,]<=beta[2]+1))/n.sim
 
 
 ## ----eval=TRUE, echo=TRUE----------------------------------------
