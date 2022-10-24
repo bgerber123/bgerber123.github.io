b326cf3050edc9982d84cfdf0293366ee894d7c1
bgerber123 <bgerber@uri.edu>
2022-10-24 12:00:26 -0400

    update

--- Week 6/linear.model.R
+++ Week 6/linear.model.R
@@ -1,15 +1,15 @@
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 # x variable (independent and known)
   x = seq(0,2,by=0.1)
   x
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
-# marginal coefficients (estimated if unknown)
-  beta0 = 0
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# marginal coefficients 
+  beta0 = 1
   beta1 = 0.5
 
-#stdev of y, (estimated if unknown) 
+# sd of y
   sigma = 0.5
 
 # Derive mu  
@@ -20,64 +20,101 @@
   set.seed(43252)
   y =  rnorm(length(mu),mean = mu, sd = sigma)
 
+
+## ---- eval=TRUE, echo=FALSE--------------------------------------
 #plot x and y  
   par(cex.axis=1.3,cex.lab=1.3)
-  plot(x,y,type="p",col=4,lwd=4,ylim=c(-2,3),
+  plot(x,y,type="p",col=4,lwd=4,ylim=c(-2,4),
        pch=18,cex=2)
   lines(x,mu,lwd=3)
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 #sample many times  
   y.many = replicate(1000,rnorm(length(mu),mu, sigma))
+  dim(y.many)
 
+
+## ---- eval=TRUE, echo=FALSE--------------------------------------
 #plot all samples with true mu
   par(cex.axis=1.3,cex.lab=1.3)
-  matplot(y.many,type="p",pch=18,xaxt="n",col=2,cex=2,xlab="x",ylab="y")
+  matplot(y.many,type="p",pch=18,xaxt="n",col=4,cex=2,xlab="x",ylab="y",ylim=c(-2,4))
   axis(1,at=1:length(x),lab=x)
   lines(1:length(x),mu,lwd=3)
   points(1:length(x),y,type="p",col=4,lwd=4,ylim=c(-2,3),
        pch=18,cex=2)
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 y <- matrix(c(1,2,3),nrow=3,ncol=1)
 y
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 x <- matrix(c(0.5,1,-2),nrow=3,ncol=1)
 x
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 p=3
 beta <- matrix(c(0,-2,2),nrow=p,ncol=1)
 beta
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
-p=2
-X <- matrix(c(1,2,3,4,5,6),nrow=3,ncol=p,byrow=FALSE)
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+p=3
+X <- matrix(c(1,2,3,4,5,6,7,8,9),nrow=3,ncol=p,byrow=FALSE)
 X
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 t(y)%*%y
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 first=t(y)
 dim(first)
 
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 second=y
 dim(second)
 
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 #When this is true
   ncol(first)==nrow(second)
 
 
-## ---- eval=TRUE,echo=TRUE------------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# GLM coefs
+  coef(glm(y~x))
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# Linear Algebra Coefs
+  X=model.matrix(~x)
+  c(solve(t(X)%*%X)%*%t(X)%*%y)
+
+
+## ---- eval=TRUE, echo=FALSE--------------------------------------
+   n=100
+   set.seed(43243)
+  x=rnorm(n,1,0.6)
+  p=0.2+0.3*x+rnorm(n,0,0.05)
+  plot(x,p,xlab="Variable",ylab="Probability",
+       ylim=c(-0.5,1.5),xlim=c(-2,3),cex=1.5)
+
+
+## ---- eval=TRUE, echo=FALSE--------------------------------------
+  plot(x,p,xlab="Variable",ylab="Probability",
+       ylim=c(-0.5,1.5),xlim=c(-2,3),cex=1.5)
+  abline(h=c(0,1),lwd=3,col='purple')
+
+  abline(glm(p~x),lwd=3,col=2)
+
+
+## ---- eval=TRUE,echo=TRUE----------------------------------------
 #Design matrix
   set.seed(6454)
   Var1 = seq(0,20,by=1)+rnorm(21,0,2)
@@ -86,53 +123,62 @@ dim(second)
   
 
 
-## ---- eval=TRUE,echo=TRUE------------------------------------------------------
+## ---- eval=TRUE,echo=TRUE----------------------------------------
 #Marginal Coefficients  
   beta=matrix(c(0,5))
 
 #linear terms
   lt = X%*%beta
 
-#mu (no transformation needed)
-  mu=lt
+#mu (link function)
+  mu=lt*1
 
 # Plot relationship b/w mean (mu) and variable of interest
-  plot(X[,2],mu,type="b",lwd=4)  
+  plot(X[,2],mu,type="l",lwd=4)  
 
 
-## ---- eval=TRUE,echo=TRUE------------------------------------------------------
+## ---- eval=TRUE,echo=TRUE----------------------------------------
 #sample
   set.seed(5435)
-  y = rnorm(length(mu),mu)
+  y = rnorm(length(mu),mu,sd=3)
   y
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE----------------------------------------
+# Plot relationship b/w mean (mu) and variable of interest
+  plot(X[,2],mu,type="l",lwd=4)  
+  points(X[,2],y,pch=18,col=2,cex=2)
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 # Fit model to sample
-  model1=glm(y~X+0,family = gaussian(link = "identity"))
+  model1=glm(y~0+X,family = gaussian(link = "identity"))
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
   summary(model1)
 
 
-## ----echo = TRUE---------------------------------------------------------------
+## ----echo = TRUE-------------------------------------------------
 library(equatiomatic)
 extract_eq(model1)
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
  #sample many times  
   y.many = replicate(1000,rnorm(length(mu),mu, sigma))
   dim(y.many)
   
  #Estimate coefs for all 100 samples
   coef.est=apply(y.many,2,FUN=function(y){
-              model1=glm(y~X+0,family = gaussian(link = "identity"))
+              model1=glm(y~0+X,family = gaussian(link = "identity"))
               model1$coefficients
   })
 
   dim(coef.est)
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=FALSE--------------------------------------
   plot(density(coef.est[1,],adjust=2),type="l",lwd=2,
        main=bquote("Sampling Distribution"~beta[0]),
        xlab=bquote(beta[0]))
@@ -143,24 +189,25 @@ extract_eq(model1)
   abline(v=beta[2],col=2,lwd=4)
 
 
-## ---- eval=TRUE,echo=TRUE, fig.align='center'----------------------------------
-p=seq(0.01,0.99,by=0.01)
+## ---- eval=TRUE,echo=TRUE, fig.align='center'--------------------
+p=seq(0.001,0.999,by=0.01)
 logit.p=qlogis(p)
 par(cex.lab=1.5,cex.axis=1.5)
 plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
 
 
-## ---- eval=TRUE,echo=TRUE, fig.align='center'----------------------------------
+
+## ---- eval=TRUE,echo=TRUE, fig.align='center'--------------------
 #Design matrix
   set.seed(43534)
-  Var1 = rnorm(20)
+  Var1 = rnorm(100)
   X = model.matrix(~Var1)
   head(X)
 
 
-## ---- eval=TRUE,echo=TRUE, fig.align='center'----------------------------------
+## ---- eval=TRUE,echo=TRUE, fig.align='center'--------------------
 # marginal coefficients (on logit-scale)
-  beta=c(-4,8)
+  beta=c(-2,4)
 
 #linear terms
   lt = X%*%beta
@@ -175,7 +222,7 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
   y
 
 
-## ---- eval=TRUE,echo=TRUE, fig.align='center'----------------------------------
+## ---- eval=TRUE,echo=TRUE, fig.align='center'--------------------
 #Plot the linear 'terms'  and probability
   par(cex.lab=1.2,cex.axis=1.2)
   plot(Var1,lt,type="b",lwd=3,col=2,
@@ -185,20 +232,21 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
   plot(Var1[index],p[index],type="b",lwd=3,col=2,xlab="x",ylab="Probability")  
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
 # Fit model to sample 
-  model1=glm(y~X+0, family = binomial(link = "logit"))
+  model1=glm(y~0+X, family = binomial(link = "logit"))
   summary(model1)
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=TRUE---------------------------------------
  #sample many times  
-  y.many = replicate(1000,rbinom(n=length(p),size=1,p))
+  n.sim=1000
+  y.many = replicate(n.sim,rbinom(n=length(p),size=1,p))
   dim(y.many)
   
  #Estimate coefs for all 100 samples
   coef.est=apply(y.many,2,FUN=function(y){
-          model1=glm(y~X+0, family = binomial(link = "logit"))
+          model1=glm(y~0+X, family = binomial(link = "logit"))
           
   model1$coefficients
   })
@@ -206,13 +254,96 @@ plot(p,logit.p,type="l",lwd=4,col=3,xlab='p',ylab="logit(p)")
   dim(coef.est)
 
 
-## ---- eval=TRUE, echo=TRUE-----------------------------------------------------
+## ---- eval=TRUE, echo=FALSE--------------------------------------
   plot(density(coef.est[1,],adjust=2),type="l",lwd=2,
        main=bquote("Sampling Distribution"~beta[0]),
        xlab=bquote(beta[0]))
       abline(v=beta[1],col=2,lwd=4)
+
+
+## ---- eval=TRUE, echo=FALSE--------------------------------------
   plot(density(coef.est[2,],adjust=2),type="l",lwd=2,
        main=bquote("Sampling Distribution"~beta[1]),
        xlab=bquote(beta[0]))
   abline(v=beta[2],col=2,lwd=4)
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# Relative Bias
+  (median(coef.est[1,])-beta[1])/beta[1]
+  (median(coef.est[2,])-beta[2])/beta[2]
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# Probability of estimating a coef of the wrong sign
+  length(which(coef.est[1,]>0))/n.sim
+  length(which(coef.est[2,]<0))/n.sim
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# Probability of estimating the slope 2x the truth
+    length(which(coef.est[2,]>2*beta[2]))/n.sim
+
+
+## ---- eval=TRUE, echo=TRUE---------------------------------------
+# Probability of estimating the slope within 1
+    length(which(coef.est[2,]>=beta[2]-1 & coef.est[2,]<=beta[2]+1))/n.sim
+
+
+## ----eval=TRUE, echo=TRUE----------------------------------------
+library(faux)
+n=100
+set.seed(543531)
+x.var <- rnorm_multi(n = n, 
+                  mu = c(10, 20),
+                  sd = c(1, 1),
+                  r = c(0), 
+                  varnames = c("A", "B"),
+                  empirical = FALSE)
+
+set.seed(54353)
+x.var.cor <- rnorm_multi(n = n, 
+                  mu = c(10, 20),
+                  sd = c(1, 1),
+                  r = c(0.8), 
+                  varnames = c("A", "B"),
+                  empirical = FALSE)
+
+#Correlation
+  cor(x.var)
+  cor(x.var.cor)
+
+
+
+## ----eval=TRUE, echo=TRUE, fig.align='center'--------------------
+par(mfrow=c(1,2))
+  plot(x.var.cor$A,x.var.cor$B)
+  plot(x.var$A,x.var$B)
+
+
+## ----eval=TRUE, echo=TRUE----------------------------------------
+
+#Design matrices
+  X.cor=model.matrix(~x.var.cor$A+x.var.cor$B)
+  X=model.matrix(~x.var$A+x.var$B)
+
+#True coefs  
+  beta=c(1,2,3)
+
+#Derive mu  
+  mu=X%*%beta
+  mu.cor=X.cor%*%beta
+
+#simulate data  
+  set.seed(54353)
+  y=rnorm(n,mu,2)
+  y.cor=rnorm(n,mu.cor,2)
+
+
+## ----eval=TRUE, echo=TRUE----------------------------------------
+summary(glm(y~0+X))
+
+
+## ----eval=TRUE, echo=TRUE----------------------------------------
+summary(glm(y~0+X.cor))
 
