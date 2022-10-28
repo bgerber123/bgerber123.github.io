f951ecea2df8c6ec81c66e336cfb8e0cc5c01c98
bgerber123 <bgerber@uri.edu>
2022-10-28 15:41:20 -0400

    update week 6 and 7

--- Week 7/bayesian.r
+++ Week 7/bayesian.r
@@ -1,4 +1,4 @@
-## ---- eval=TRUE,echo=FALSE---------------------------------------------------
+## ---- eval=TRUE,echo=FALSE--------------------------------
 set.seed(923874)                 # Create example data
 data <- data.frame(x = c("Site 1","Site 2"),
                          Pop.Size = c(75,100),
@@ -12,13 +12,13 @@ ggplot(data, aes(x, Pop.Size)) +        # ggplot2 plot with confidence intervals
 
 
 
-## ---- eval=TRUE,echo=FALSE---------------------------------------------------
+## ---- eval=TRUE,echo=FALSE--------------------------------
 par(mfrow=c(1,2))
 hist(rnorm(10000,75,20),xlim=c(0,200),freq=FALSE,main="Posterior Probability Distribution",xlab="Population Size at Site 1")
 curve(dnorm(x,75,20),xlim=c(0,200),main="Posterior Probability Distribution",ylab="Density",lwd=3,xlab="Population Size at Site 1")
 
 
-## ---- eval=TRUE,echo=FALSE---------------------------------------------------
+## ---- eval=TRUE,echo=FALSE--------------------------------
 par(mfrow=c(1,2))
 set.seed(546543)
 x=rnorm(10000,75,20)
@@ -29,7 +29,7 @@ curve(dnorm(x,75,20),xlim=c(0,200),main="Posterior Probability Distribution",yla
 abline(v=mean(x),lwd=3,col=2)
 
 
-## ---- eval=TRUE,echo=FALSE---------------------------------------------------
+## ---- eval=TRUE,echo=FALSE--------------------------------
 par(mfrow=c(1,2))
 set.seed(4345)
 post1=rnorm(10000,75,15)
@@ -42,21 +42,21 @@ hist(diff,freq=FALSE,col=3,main="Posterior Distributions \nof the difference in 
      xlab="Pop Size 2 - Pop Size 1")
 
 
-## ---- eval=TRUE,echo=TRUE----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE---------------------------------
 length(which(diff>0))/length(diff)
 
 
-## ---- eval=TRUE,echo=FALSE---------------------------------------------------
+## ---- eval=TRUE,echo=FALSE--------------------------------
 set.seed(5435)
 x=seq(-1,1,by=0.1)
 y=rnorm(21,mean=1+1*x,sd=1)
 
 
-## ---- eval=TRUE,echo=TRUE----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE---------------------------------
 summary(glm(y~x))
 
 
-## ---- eval=TRUE,echo=FALSE---------------------------------------------------
+## ---- eval=TRUE,echo=FALSE--------------------------------
 set.seed(543534)
 post=rnorm(1000,0.5,1)
 hist(post,main="Posterior Distribution of Effect of Elevation",freq = FALSE,xlab="Slope/Coeficient")
@@ -65,32 +65,32 @@ abline(v=mean(post), lwd=3,col=3)
 legend("topleft",col=c(2,3),lwd=3,legend=c("No Effect", "Posterior Mean"))
 
 
-## ---- eval=TRUE,echo=TRUE----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE---------------------------------
 #Posterior Mean
   mean(post)
 
 
-## ---- eval=TRUE,echo=TRUE----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE---------------------------------
 #Credible/Probability Intervals 
   quantile(post,prob=c(0.025,0.975))
 
 
-## ---- eval=TRUE,echo=TRUE----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE---------------------------------
 # #Probabilty of a postive effect
  length(which(post>0))/length(post)
 
 
-## ---- eval=TRUE,echo=TRUE----------------------------------------------------
+## ---- eval=TRUE,echo=TRUE---------------------------------
 # #Probabilty of a negative effect
  length(which(post<0))/length(post)
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
+## ----eval=TRUE, echo=TRUE---------------------------------
 curve(dbeta(x,1,1),xlim=c(0,1),lwd=3,col=2,xlab="p",
       ylab = "Prior Probability Density")
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
+## ----eval=TRUE, echo=TRUE---------------------------------
 # Survival outcomes of three adult hippos
   y1=c(0,0,0,0,0,0,0,1,1)
   N1=length(y1)
@@ -98,124 +98,124 @@ curve(dbeta(x,1,1),xlim=c(0,1),lwd=3,col=2,xlab="p",
   mle.p
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
-  alpha1=1
-  beta1=1
+## ----eval=TRUE, echo=TRUE---------------------------------
+  alpha.prior1=1
+  beta.prior1=1
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 #Plot of prior 1
-curve(dbeta(x,shape1=alpha1,shape2=beta1),lwd=3,
+curve(dbeta(x,shape1=alpha.prior1,shape2=beta.prior1),lwd=3,
       xlab="Probability",ylab="Probabilty Density",
       main="Prior Probability of Success",ylim=c(0,20))
 legend("topleft",col=c(1,2),legend=c("Prior 1"),lwd=3)
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
-  alpha2=10
-  beta2=2
+## ----eval=TRUE, echo=TRUE---------------------------------
+  alpha.prior2=10
+  beta.prior2=2
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
-curve(dbeta(x,shape1=alpha1,shape2=beta1),lwd=3,
+## ----eval=TRUE, echo=FALSE--------------------------------
+curve(dbeta(x,shape1=alpha.prior1,shape2=beta.prior1),lwd=3,
       xlab="Probability",ylab="Probabilty Density",
       main="Prior Probability of Success",ylim=c(0,20))
 legend("topleft",col=c(1,2),legend=c("Prior 1"),lwd=3)
-curve(dbeta(x,shape1=alpha2,shape2=beta2),lwd=3,col=2,add=TRUE)
+curve(dbeta(x,shape1=alpha.prior2,shape2=beta.prior2),lwd=3,col=2,add=TRUE)
 legend("topleft",col=c(1,2),legend=c("Prior 1", "Prior 2"),lwd=3)
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
-  alpha3=150
-  beta3=15
+## ----eval=TRUE, echo=TRUE---------------------------------
+  alpha.prior3=150
+  beta.prior3=15
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
-curve(dbeta(x,shape1=alpha1,shape2=beta1),lwd=3,
+## ----eval=TRUE, echo=FALSE--------------------------------
+curve(dbeta(x,shape1=alpha.prior1,shape2=beta.prior1),lwd=3,
       xlab="Probability",ylab="Probabilty Density",
       main="Prior Probability of Success",ylim=c(0,20))
 legend("topleft",col=c(1,2),legend=c("Prior 1"),lwd=3)
-curve(dbeta(x,shape1=alpha2,shape2=beta2),lwd=3,col=2,add=TRUE)
-curve(dbeta(x,shape1=alpha3,shape2=beta3),lwd=3,col=3,add=TRUE)
+curve(dbeta(x,shape1=alpha.prior2,shape2=beta.prior2),lwd=3,col=2,add=TRUE)
+curve(dbeta(x,shape1=alpha.prior3,shape2=beta.prior3),lwd=3,col=3,add=TRUE)
 legend("topleft",col=c(1,2,3),legend=c("Prior 1", "Prior 2","Prior 3"),lwd=3)
 
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
+## ----eval=TRUE, echo=TRUE---------------------------------
 # Note- the data are the same, but the prior is changing
-  post.1=rbeta(10000,alpha1+sum(y1),beta1+N1-sum(y1))
-  post.2=rbeta(10000,alpha2+sum(y1),beta2+N1-sum(y1))
-  post.3=rbeta(10000,alpha3+sum(y1),beta3+N1-sum(y1))
+  post.1=rbeta(10000,alpha.prior1+sum(y1),beta.prior1+N1-sum(y1))
+  post.2=rbeta(10000,alpha.prior2+sum(y1),beta.prior2+N1-sum(y1))
+  post.3=rbeta(10000,alpha.prior3+sum(y1),beta.prior3+N1-sum(y1))
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 plot(density(post.1),ylim=c(0,20),xlim=c(0,1),col=1,lwd=3,main="Prior 1",
      xlab="Posterior Probability",ylab="Probability Density")
-curve(dbeta(x,shape1=alpha1,shape2=beta1),
+curve(dbeta(x,shape1=alpha.prior1,shape2=beta.prior1),
       add=TRUE,col=1,lwd=3,lty=3)
 abline(v=mle.p,col="purple",lwd=3)
 legend("topright",lwd=3,lty=c(1,3,1),col=c("black","black","purple"),
        legend=c("Posterior","Prior","MLE"))
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 plot(density(post.2),ylim=c(0,20),xlim=c(0,1),col=2,lwd=3,main="Prior 2",
      xlab="Posterior Probability",ylab="Probability Density")
-curve(dbeta(x,shape1=alpha2,shape2=beta2),
+curve(dbeta(x,shape1=alpha.prior2,shape2=beta.prior2),
       add=TRUE,col=2,lwd=3,lty=3)
 abline(v=mle.p,col="purple",lwd=3)
 legend("topright",lwd=3,lty=c(1,3,1),col=c("red","red","purple"),
        legend=c("Posterior","Prior","MLE"))
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 plot(density(post.3),ylim=c(0,20),xlim=c(0,1),col=3,lwd=3,main="Prior 3",
      xlab="Posterior Probability",ylab="Probability Density")
-curve(dbeta(x,shape1=alpha3,shape2=beta3),
+curve(dbeta(x,shape1=alpha.prior3,shape2=beta.prior3),
       add=TRUE,col=3,lwd=3,lty=3)
 abline(v=mle.p,col="purple",lwd=3)
 legend("topleft",lwd=3,lty=c(1,3,1),col=c("green","green","purple"),
        legend=c("Posterior","Prior","MLE"))
 
 
-## ----eval=TRUE, echo=TRUE----------------------------------------------------
+## ----eval=TRUE, echo=TRUE---------------------------------
 y2=c(0,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,
      1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0)
 length(y2)
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 N2=length(y2)
 
 mle.p=mean(y2)
 
-post.4=rbeta(10000,alpha1+sum(y2),beta1+N2-sum(y2))
-post.5=rbeta(10000,alpha2+sum(y2),beta2+N2-sum(y2))
-post.6=rbeta(10000,alpha3+sum(y2),beta3+N2-sum(y2))
+post.4=rbeta(10000,alpha.prior1+sum(y2),beta.prior1+N2-sum(y2))
+post.5=rbeta(10000,alpha.prior2+sum(y2),beta.prior2+N2-sum(y2))
+post.6=rbeta(10000,alpha.prior3+sum(y2),beta.prior3+N2-sum(y2))
 
 plot(density(post.4),ylim=c(0,20),xlim=c(0,1),col=1,lwd=3,main="Prior 1",
      xlab="Posterior Probability",ylab="Probability Density")
-curve(dbeta(x,shape1=alpha1,shape2=beta1),
+curve(dbeta(x,shape1=alpha.prior1,shape2=beta.prior1),
       add=TRUE,col=1,lwd=3,lty=3)
 abline(v=mle.p,col="purple",lwd=3)
 legend("topleft",lwd=3,lty=c(1,3,1),col=c("black","black","purple"),
        legend=c("Posterior","Prior","MLE"))
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 plot(density(post.5),ylim=c(0,20),xlim=c(0,1),col=2,lwd=3,main="Prior 2",
      xlab="Posterior Probability",ylab="Probability Density")
-curve(dbeta(x,shape1=alpha2,shape2=beta2),
+curve(dbeta(x,shape1=alpha.prior2,shape2=beta.prior2),
       add=TRUE,col=2,lwd=3,lty=3)
 abline(v=mle.p,col="purple",lwd=3)
 legend("topleft",lwd=3,lty=c(1,3,1),col=c("red","red","purple"),
        legend=c("Posterior","Prior","MLE"))
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 plot(density(post.6),ylim=c(0,20),xlim=c(0,1),col=3,lwd=3,main="Prior 3",
      xlab="Posterior Probability",ylab="Probability Density")
-curve(dbeta(x,shape1=alpha3,shape2=beta3),
+curve(dbeta(x,shape1=alpha.prior3,shape2=beta.prior3),
       add=TRUE,col=3,lwd=3,lty=3)
 abline(v=mle.p,col="purple",lwd=3)
 legend("topleft",lwd=3,lty=c(1,3,1),col=c("green","green","purple"),
@@ -223,7 +223,7 @@ legend("topleft",lwd=3,lty=c(1,3,1),col=c("green","green","purple"),
 
 
 
-## ----eval=TRUE, echo=FALSE---------------------------------------------------
+## ----eval=TRUE, echo=FALSE--------------------------------
 par(mfrow=c(1,2))
 plot(density(post.1),ylim=c(0,20),xlim=c(0,1),col=1,lwd=3,main="Small Data",
      xlab="Posterior Probability",ylab="Probability Density")
