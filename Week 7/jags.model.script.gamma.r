944952d3c832d3b4435ee8cab24b072f83b15905
bgerber123 <bgerber@uri.edu>
2022-11-21 13:21:17 -0500

    update week 9

--- Week 7/jags.model.script.gamma.r
+++ Week 7/jags.model.script.gamma.r
@@ -32,3 +32,41 @@ post=coda.samples(jm, variable.names=parms, n.iter=n.iter, thin=thin)
 mcmc_areas(as.matrix(post))
 
 hist(as.matrix(post))
+
+
+#############################
+#Conjugate Poisson Likelihood with gamma prior
+
+mean(y)
+mean(y2)
+#Gamma parameters
+  #alpha = shape
+  #beta = rate
+
+#shape and rate
+alpha=1
+beta=1
+
+curve(dgamma(x,shape=alpha,rate=beta),xlim=c(0,10))
+
+alpha.star=alpha+sum(y)
+beta.star=beta+length(y)
+
+posterior1=rgamma(1000,shape=alpha.star,rate=beta.star)
+hist(posterior1,freq=FALSE)
+
+#####################################################
+#shape and scale
+k=alpha
+theta=1/beta
+
+curve(dgamma(x,shape=k,scale=theta),xlim=c(0,10))
+
+k.star=k+sum(y)
+theta.star=theta/(length(y)*theta+1)
+posterior2=rgamma(1000,shape=k.star,scale=theta.star)
+
+hist(posterior2,add=TRUE,freq=FALSE,col=2)
+
+
+
