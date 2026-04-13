03015aa1d1cb8f1d89f5029be917606f573dc793
bgerber123 <brian.gerber@colostat.edu>
2026-04-13 15:58:19 -0600

    fw670

--- FW670/BigPicture_code.R
+++ FW670/BigPicture_code.R
@@ -1,4 +1,4 @@
-## ----design.sim, echo=TRUE, eval=TRUE-----------------------------------------------------------------------------------
+## ----design.sim, echo=TRUE, eval=TRUE-------------------------
 #random discrete uniform sampler
 rdu<-function(n,lower,upper){sample(lower:upper,n,replace=T)}
 
@@ -11,7 +11,7 @@ mat = matrix(rdu(25,
 mat
 
 
-## ----design.sim2, echo=TRUE, eval=TRUE----------------------------------------------------------------------------------
+## ----design.sim2, echo=TRUE, eval=TRUE------------------------
   n = 10
   y = sample(
              c(mat),
@@ -21,12 +21,12 @@ mat
   y
 
 
-## ----design.sim4, echo=TRUE, eval=TRUE----------------------------------------------------------------------------------
+## ----design.sim4, echo=TRUE, eval=TRUE------------------------
   mean(y)
 
 
 
-## ----sampling dist, eval=TRUE, echo=TRUE--------------------------------------------------------------------------------
+## ----sampling dist, eval=TRUE, echo=TRUE----------------------
 # How many ways can we uniquely sample 10 things from 25
 combs = function(n, x) {
   factorial(n) / factorial(n-x) / factorial(x)
@@ -35,14 +35,14 @@ combs = function(n, x) {
 combs(25, 10)
 
 
-## ----sampling dist2, eval=TRUE, echo=TRUE, cache=TRUE-------------------------------------------------------------------
+## ----sampling dist2, eval=TRUE, echo=TRUE, cache=TRUE---------
   set.seed(5435)
   all.combs = utils::combn(c(mat), 10)
   dim(all.combs)
   mean.all.combs = apply(all.combs,2,mean)
 
 
-## ----sampling dist3, eval=TRUE, echo=TRUE, cache=TRUE-------------------------------------------------------------------
+## ----sampling dist3, eval=TRUE, echo=TRUE, cache=TRUE---------
   set.seed(5435)
   sim.sampling.dist=replicate(2000,
                               sample(c(mat),10)
@@ -51,7 +51,7 @@ combs(25, 10)
   mean.samples = apply(sim.sampling.dist,2,mean)
 
 
-## ----sampling dist4, eval=TRUE, echo=FALSE------------------------------------------------------------------------------
+## ----sampling dist4, eval=TRUE, echo=FALSE--------------------
   hist(mean.all.combs,
        freq=FALSE, 
        main="Sampling Distributiuon of the Mean",
@@ -67,7 +67,7 @@ combs(25, 10)
   legend("topright",lwd=5,col=c(grDevices::adjustcolor("black", alpha.f=0.5),grDevices::adjustcolor("red", alpha.f=0.5)),legend = c("Complete", "Approximated"))
 
 
-## ----model.sim, echo=TRUE, eval=TRUE------------------------------------------------------------------------------------
+## ----model.sim, echo=TRUE, eval=TRUE--------------------------
 #Create a function, to be replicated
   lambda=200
   n.sim=500
@@ -77,7 +77,7 @@ combs(25, 10)
   }
 
 
-## ----model.sim2, echo=TRUE, eval=TRUE-----------------------------------------------------------------------------------
+## ----model.sim2, echo=TRUE, eval=TRUE-------------------------
 # repeat the function n.sim times
   list.mat = replicate(n.sim, 
                        mat.fn(lambda), 
@@ -86,7 +86,7 @@ combs(25, 10)
   length(list.mat)
 
 
-## ----model.sim3, echo=TRUE, eval=TRUE-----------------------------------------------------------------------------------
+## ----model.sim3, echo=TRUE, eval=TRUE-------------------------
 #One realization
   list.mat[[1]]
 
@@ -95,18 +95,19 @@ combs(25, 10)
   mean(list.mat[[1]])
 
 
-## ----model.sim4, echo=TRUE, eval=TRUE-----------------------------------------------------------------------------------
+## ----model.sim4, echo=TRUE, eval=TRUE-------------------------
 lambda.hat = unlist(lapply(list.mat,FUN=mean))
 
 
-## ----model.sim5, echo=FALSE, eval=TRUE----------------------------------------------------------------------------------
+## ----model.sim5, echo=FALSE, eval=TRUE------------------------
 hist(lambda.hat,
      xlab=bquote(lambda-hat),
      main=bquote("Sampling Distribution of"~lambda-hat)
      )
+#abline(v=lambda, lwd=3, col=4)
 
 
-## ----bias22,eval=TRUE, echo=TRUE----------------------------------------------------------------------------------------
+## ----bias22,eval=TRUE, echo=TRUE------------------------------
 # Bias
   mean(lambda.hat) - lambda
 
@@ -114,14 +115,14 @@ hist(lambda.hat,
   (mean(lambda.hat) - lambda)/lambda
 
 
-## ----bias3,eval=TRUE, echo=FALSE----------------------------------------------------------------------------------------
+## ----bias3,eval=TRUE, echo=FALSE------------------------------
   hist(lambda.hat,xlab=bquote(lambda), breaks=20)
   abline(v=mean(lambda.hat),lwd=4,col=4,lty=2)
   abline(v=lambda,lwd=4,col=3,lty=3)
   legend("topright",legend=c("mean","truth"),lwd=3,col=c(4,3),lty=c(2,3))
 
 
-## ----bias4,eval=TRUE, echo=TRUE-----------------------------------------------------------------------------------------
+## ----bias4,eval=TRUE, echo=TRUE-------------------------------
   diff = 0.025*lambda
   diff
   lower = lambda-diff
---
+++ docs/FW670/BigPicture_code.R
@@ -0,0 +1,135 @@
+## ----design.sim, echo=TRUE, eval=TRUE-------------------------
+#random discrete uniform sampler
+rdu<-function(n,lower,upper){sample(lower:upper,n,replace=T)}
+
+mat = matrix(rdu(25, 
+                 lower = 0, 
+                 upper = 400
+                 ),
+             nrow=5, ncol=5
+             )
+mat
+
+
+## ----design.sim2, echo=TRUE, eval=TRUE------------------------
+  n = 10
+  y = sample(
+             c(mat),
+             10, 
+             replace = TRUE
+             )
+  y
+
+
+## ----design.sim4, echo=TRUE, eval=TRUE------------------------
+  mean(y)
+
+
+
+## ----sampling dist, eval=TRUE, echo=TRUE----------------------
+# How many ways can we uniquely sample 10 things from 25
+combs = function(n, x) {
+  factorial(n) / factorial(n-x) / factorial(x)
+}
+
+combs(25, 10)
+
+
+## ----sampling dist2, eval=TRUE, echo=TRUE, cache=TRUE---------
+  set.seed(5435)
+  all.combs = utils::combn(c(mat), 10)
+  dim(all.combs)
+  mean.all.combs = apply(all.combs,2,mean)
+
+
+## ----sampling dist3, eval=TRUE, echo=TRUE, cache=TRUE---------
+  set.seed(5435)
+  sim.sampling.dist=replicate(2000,
+                              sample(c(mat),10)
+                              )
+  dim(sim.sampling.dist)
+  mean.samples = apply(sim.sampling.dist,2,mean)
+
+
+## ----sampling dist4, eval=TRUE, echo=FALSE--------------------
+  hist(mean.all.combs,
+       freq=FALSE, 
+       main="Sampling Distributiuon of the Mean",
+       breaks=10,
+       xlab="Means"
+       )
+  hist(mean.samples,
+       add=TRUE,
+       freq=FALSE,
+       col=grDevices::adjustcolor("red", alpha.f=0.5),
+       breaks=10
+       )
+  legend("topright",lwd=5,col=c(grDevices::adjustcolor("black", alpha.f=0.5),grDevices::adjustcolor("red", alpha.f=0.5)),legend = c("Complete", "Approximated"))
+
+
+## ----model.sim, echo=TRUE, eval=TRUE--------------------------
+#Create a function, to be replicated
+  lambda=200
+  n.sim=500
+  mat.fn = function(lambda){matrix(rpois(25, lambda=lambda),
+                               nrow=5, ncol=5
+                               )
+  }
+
+
+## ----model.sim2, echo=TRUE, eval=TRUE-------------------------
+# repeat the function n.sim times
+  list.mat = replicate(n.sim, 
+                       mat.fn(lambda), 
+                       simplify=FALSE
+                       )
+  length(list.mat)
+
+
+## ----model.sim3, echo=TRUE, eval=TRUE-------------------------
+#One realization
+  list.mat[[1]]
+
+
+# Sample mean for the first realization
+  mean(list.mat[[1]])
+
+
+## ----model.sim4, echo=TRUE, eval=TRUE-------------------------
+lambda.hat = unlist(lapply(list.mat,FUN=mean))
+
+
+## ----model.sim5, echo=FALSE, eval=TRUE------------------------
+hist(lambda.hat,
+     xlab=bquote(lambda-hat),
+     main=bquote("Sampling Distribution of"~lambda-hat)
+     )
+#abline(v=lambda, lwd=3, col=4)
+
+
+## ----bias22,eval=TRUE, echo=TRUE------------------------------
+# Bias
+  mean(lambda.hat) - lambda
+
+# relative bias
+  (mean(lambda.hat) - lambda)/lambda
+
+
+## ----bias3,eval=TRUE, echo=FALSE------------------------------
+  hist(lambda.hat,xlab=bquote(lambda), breaks=20)
+  abline(v=mean(lambda.hat),lwd=4,col=4,lty=2)
+  abline(v=lambda,lwd=4,col=3,lty=3)
+  legend("topright",legend=c("mean","truth"),lwd=3,col=c(4,3),lty=c(2,3))
+
+
+## ----bias4,eval=TRUE, echo=TRUE-------------------------------
+  diff = 0.025*lambda
+  diff
+  lower = lambda-diff
+  upper = lambda+diff
+
+  index = which(lambda.hat>=lower & lambda.hat <= upper)
+
+#Probability of getting a mean within 5% of the truth
+  length(index)/n.sim
+
