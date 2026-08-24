31d87d17bf736515ee11bbc222f9193b08fd9827
bgerber123 <brian.gerber@colostat.edu>
2026-08-24 12:33:35 -0600

    first week update

--- FW670/BigPicture_code.R
+++ FW670/BigPicture_code.R
@@ -1,128 +1,176 @@
-## ----design.sim, echo=TRUE, eval=TRUE-------------------------
-#random discrete uniform sampler
-rdu<-function(n,lower,upper){sample(lower:upper,n,replace=T)}
+## ----echo=TRUE, eval=TRUE--------------------------------
+# Random discrete uniform sampler
+rdu <- function(n, lower, upper){
+                               sample(lower:upper,
+                                      n,
+                                      replace=TRUE
+                                      )
+                              }
+N = 25
 
-mat = matrix(rdu(25, 
+# This is our truth, which we will sample from
+set.seed(4343)
+mat = matrix(rdu(N, 
                  lower = 0, 
                  upper = 400
                  ),
-             nrow=5, ncol=5
+             nrow = 5, 
+             ncol = 5
              )
+
+
+## ----echo=FALSE, eval=TRUE-------------------------------
 mat
 
 
-## ----design.sim2, echo=TRUE, eval=TRUE------------------------
+## ----echo=TRUE, eval=TRUE--------------------------------
+mean(mat)
+
+
+## ----echo=TRUE, eval=TRUE--------------------------------
+sum(0:400 * 1/401)
+
+
+## ----echo=TRUE, eval=TRUE--------------------------------
   n = 10
   y = sample(
              c(mat),
-             10, 
+             n, 
              replace = TRUE
              )
   y
 
 
-## ----design.sim4, echo=TRUE, eval=TRUE------------------------
+## ----echo=TRUE, eval=TRUE--------------------------------
   mean(y)
 
 
-
-## ----sampling dist, eval=TRUE, echo=TRUE----------------------
+## ----sampling dist, eval=TRUE, echo=TRUE-----------------
 # How many ways can we uniquely sample 10 things from 25
 combs = function(n, x) {
   factorial(n) / factorial(n-x) / factorial(x)
 }
 
-combs(25, 10)
+combs(N, n)
 
 
-## ----sampling dist2, eval=TRUE, echo=TRUE, cache=TRUE---------
-  set.seed(5435)
-  all.combs = utils::combn(c(mat), 10)
+## ----sampling dist2, eval=TRUE, echo=TRUE, cache=TRUE----
+  set.seed(5411)
+  all.combs = utils::combn(c(mat), 
+                           n,
+                           simplify = TRUE
+                           )
   dim(all.combs)
-  mean.all.combs = apply(all.combs,2,mean)
+  mean.all.combs = apply(all.combs,
+                         2,
+                         mean
+                         )
 
 
-## ----sampling dist3, eval=TRUE, echo=TRUE, cache=TRUE---------
-  set.seed(5435)
-  sim.sampling.dist=replicate(2000,
-                              sample(c(mat),10)
-                              )
-  dim(sim.sampling.dist)
-  mean.samples = apply(sim.sampling.dist,2,mean)
+## ----eval=TRUE, echo=FALSE-------------------------------
 
-
-## ----sampling dist4, eval=TRUE, echo=FALSE--------------------
   hist(mean.all.combs,
        freq=FALSE, 
        main="Sampling Distributiuon of the Mean",
        breaks=10,
        xlab="Means"
        )
+legend("topright",legend=c("Truth","Mean of means"),lwd=3,col=c(2,3))
+abline(v=mean(mat),col=2,lwd=3)
+abline(v=mean(mean.all.combs),col=3,lwd=3,lty=3)
+
+
+
+## ----sampling dist3, eval=TRUE, echo=TRUE, cache=TRUE----
+  set.seed(54352)
+  sim.sampling.dist=replicate(3000,
+                              sample(c(mat),n)
+                              )
+  
+  dim(sim.sampling.dist)
+  mean.samples = apply(sim.sampling.dist,2,mean)
+
+
+## ----sampling dist4, eval=TRUE, echo=FALSE---------------
+  hist(mean.all.combs,
+       freq=FALSE, 
+       main="Sampling Distributiuon of the Mean",
+       breaks=20,
+       xlab="Means"
+       )
   hist(mean.samples,
        add=TRUE,
        freq=FALSE,
        col=grDevices::adjustcolor("red", alpha.f=0.5),
-       breaks=10
+       breaks=20
        )
+  # legend("topright",legend=c("Truth","Mean of means"),lwd=3,col=c(2,3))
+  abline(v=mean(mat),col=3,lwd=3)
+  abline(v=mean(mean.samples),col=4,lwd=3,lty=3)
+
   legend("topright",lwd=5,col=c(grDevices::adjustcolor("black", alpha.f=0.5),grDevices::adjustcolor("red", alpha.f=0.5)),legend = c("Complete", "Approximated"))
 
 
-## ----model.sim, echo=TRUE, eval=TRUE--------------------------
-#Create a function, to be replicated
-  lambda=200
-  n.sim=500
-  mat.fn = function(lambda){matrix(rpois(25, lambda=lambda),
-                               nrow=5, ncol=5
-                               )
-  }
+## ----model.sim, echo=TRUE, eval=TRUE---------------------
+# Create a function, to be replicated
+  lambda = 200
+
+  mat.fn = function(lambda){matrix(
+                                   rpois(N, lambda = lambda),
+                                   nrow = 5, 
+                                   ncol = 5
+                                  )
+                           }
 
 
-## ----model.sim2, echo=TRUE, eval=TRUE-------------------------
-# repeat the function n.sim times
+## ----model.sim2, echo=TRUE, eval=TRUE--------------------
+# Repeat the function n.sim times
+  n.sim = 1000
   list.mat = replicate(n.sim, 
                        mat.fn(lambda), 
-                       simplify=FALSE
+                       simplify = FALSE
                        )
   length(list.mat)
 
 
-## ----model.sim3, echo=TRUE, eval=TRUE-------------------------
-#One realization
+## ----model.sim3, echo=TRUE, eval=TRUE--------------------
+# One realization from the super-population
   list.mat[[1]]
 
 
-# Sample mean for the first realization
+# Population mean for the first realization
   mean(list.mat[[1]])
 
 
-## ----model.sim4, echo=TRUE, eval=TRUE-------------------------
-lambda.hat = unlist(lapply(list.mat,FUN=mean))
+## ----model.sim4,echo=TRUE, eval=TRUE---------------------
+samples.list = lapply(list.mat,FUN=function(x){sample(x,size=n)})
+lambda.hat = unlist(lapply(samples.list,FUN=mean))
 
 
-## ----model.sim5, echo=FALSE, eval=TRUE------------------------
+## ----model.sim5, echo=FALSE, eval=TRUE-------------------
 hist(lambda.hat,
      xlab=bquote(lambda-hat),
-     main=bquote("Sampling Distribution of"~lambda-hat)
+     main=bquote("Sampling distribution of"~hat(lambda))
      )
-#abline(v=lambda, lwd=3, col=4)
+abline(v=lambda, lwd=3, col=4)
 
 
-## ----bias22,eval=TRUE, echo=TRUE------------------------------
-# Bias
+## ----bias22,eval=TRUE, echo=TRUE-------------------------
+# Bias (in this case, Monte Carlo error)
   mean(lambda.hat) - lambda
 
 # relative bias
   (mean(lambda.hat) - lambda)/lambda
 
 
-## ----bias3,eval=TRUE, echo=FALSE------------------------------
+## ----bias3,eval=TRUE, echo=FALSE-------------------------
   hist(lambda.hat,xlab=bquote(lambda), breaks=20)
   abline(v=mean(lambda.hat),lwd=4,col=4,lty=2)
   abline(v=lambda,lwd=4,col=3,lty=3)
   legend("topright",legend=c("mean","truth"),lwd=3,col=c(4,3),lty=c(2,3))
 
 
-## ----bias4,eval=TRUE, echo=TRUE-------------------------------
+## ----bias4,eval=TRUE, echo=TRUE--------------------------
   diff = 0.025*lambda
   diff
   lower = lambda-diff
@@ -130,6 +178,6 @@ hist(lambda.hat,
 
   index = which(lambda.hat>=lower & lambda.hat <= upper)
 
-#Probability of getting a mean within 5% of the truth
-  length(index)/n.sim
+# Probability of getting a mean within 5% of the truth
+  length(index)/length(lambda.hat)
 
--- FW670/pdf.output.from.quarto.R
+++ FW670/pdf.output.from.quarto.R
@@ -1,19 +1,19 @@
 fi = list.file()
 
-quarto::quarto_render("introduction.qmd", output_format = "pdf", quiet=FALSE)
+quarto::quarto_render("./FW670/introduction.qmd", output_format = "pdf", quiet=FALSE)
 
-quarto::quarto_render("BigPicture.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/BigPicture.qmd", output_format = "pdf")
 
-quarto::quarto_render("Probability.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/Probability.qmd", output_format = "pdf")
 
-quarto::quarto_render("likelihood.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/likelihood.qmd", output_format = "pdf")
 
-quarto::quarto_render("regression.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/regression.qmd", output_format = "pdf")
 
-quarto::quarto_render("glm1.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/glm1.qmd", output_format = "pdf")
 
-quarto::quarto_render("glm2.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/glm2.qmd", output_format = "pdf")
 
-quarto::quarto_render("hierarchical.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/hierarchical.qmd", output_format = "pdf")
 
-quarto::quarto_render("bayesian.qmd", output_format = "pdf")
+quarto::quarto_render("./FW670/bayesian.qmd", output_format = "pdf")
